6f41bf3a3cd2bb3b83e37993741419d3f4d9fe0c
Tim Dobson <hello@tdobson.net>
2024-09-17 17:57:55 +0100

    feat: Update jtl_member_db_stats view

--- views/jtl_member_db_stats.sql
+++ views/jtl_member_db_stats.sql
@@ -4,7 +4,94 @@ select
   count(
     distinct case
       when `o`.`cc_attendance` in ('attended', 'no-register-show', 'noregistershow')
+      and `o`.`cc_volunteer` not in('none', '')
+      and `o`.`cc_volunteer_attendance` in (
+        'attended',
+        'completed',
+        'did it',
+        'attend',
+        'pending',
+        ''
+      )
+      and `o`.`user_id` = `u`.`id`
       and `o`.`status` = 'wc-completed' then `o`.`order_item_name`
+      else case
+        when `o`.`cc_attendance` in (
+          'cancelled',
+          'bail',
+          'no-show',
+          'late-bail',
+          'latebail',
+          'noshow',
+          'cancelled by the Clan',
+          'cancelled due to bad weather'
+        )
+        and `o`.`cc_volunteer` not in('none', '')
+        and `o`.`cc_volunteer_attendance` in ('attended', 'completed', 'did it', 'attend')
+        and `o`.`user_id` = `u`.`id`
+        and `o`.`status` = 'wc-completed' then `o`.`order_item_name`
+      end
+    end
+  ) AS `volunteer_for_numerator`,
+  count(
+    distinct case
+      when `o`.`cc_attendance` in (
+        'cancelled',
+        'bail',
+        'no-show',
+        'late-bail',
+        'latebail',
+        'noshow'
+      )
+      and `o`.`cc_volunteer` not in('none', '')
+      and `o`.`cc_volunteer_attendance` not in('attended', 'completed', 'did it', 'attend') then `o`.`order_item_name`
+    end
+  ) AS `volunteer_for_but_no_attend`,
+  count(
+    distinct case
+      when `o`.`cc_volunteer_attendance` in (
+        'cancelled',
+        'bail',
+        'no-show',
+        'late-bail',
+        'noshow',
+        'cancel',
+        'latebail'
+      )
+      and `o`.`cc_volunteer` not in('none', '', 'pending')
+      and `o`.`cc_attendance` in ('attended', 'noregistershow', 'no-register-show') then `o`.`order_item_name`
+    end
+  ) AS `volunteer_for_but_no_volunteer`,
+  count(
+    distinct case
+      when `o`.`cc_attendance` in (
+        'cancelled',
+        'bail',
+        'no-show',
+        'late-bail',
+        'latebail',
+        'noshow',
+        'cancelled by the Clan',
+        'cancelled due to bad weather'
+      )
+      and `o`.`cc_volunteer` not in('none', '')
+      and `o`.`cc_volunteer_attendance` in ('attended', 'completed', 'did it', 'attend') then `o`.`order_item_name`
+    end
+  ) AS `volunteer_for_and_cancel_but_did_volunteer`,
+  count(
+    distinct case
+      when `o`.`cc_attendance` in (
+        'cancelled',
+        'bail',
+        'no-show',
+        'late-bail',
+        'attended'
+      ) then `o`.`order_item_name`
+    end
+  ) AS `volunteer_for_denominator`,
+  count(
+    distinct case
+      when `o`.`cc_attendance` = 'attended' then `o`.`order_item_name`
     end
   ) AS `attendance_attended`,
   count(
@@ -32,7 +119,7 @@ select
       ) then `o`.`order_item_name`
     end
   ) AS `attendance_day_attended`,
-    count(
+  count(
     distinct case
       when `o`.`cc_attendance` = 'attended'
       and (
@@ -90,6 +177,20 @@ select
       ) then `o`.`order_item_name`
     end
   ) AS `attendance_noshow`,
+  count(
+    distinct case
+      when `o`.`cc_attendance` in ('cancelled due to bad weather', 'weather-cancel') then `o`.`order_item_name`
+    end
+  ) AS `attendance_weather_cancel`,
+  count(
+    distinct case
+      when `o`.`cc_attendance` in (
+        'cancelled by the Clan',
+        'clancancel',
+        'clan-cancel'
+      ) then `o`.`order_item_name`
+    end
+  ) AS `attendance_clan_cancel`,
   count(
     distinct case
       when `o`.`cc_attendance` in ('late-bail', 'latebail') then `o`.`order_item_name`
